6cc006f483c0050c1c7603213b783e8602a475cd
Nick <zinomast@gmail.com>
2016-02-10 00:12:04 +0200

    Skeleton: WebAPI template
    Created project based on WepAPI2 template

---
+++ WebDiskBrowser/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebDiskBrowser.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebDiskBrowser/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebDiskBrowser.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
