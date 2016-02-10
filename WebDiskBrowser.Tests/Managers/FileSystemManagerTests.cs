6bbb7c4c07d5f8f952b53f7710e536140692a9cd
Nick <zinomast@gmail.com>
2016-02-10 19:27:47 +0200

    WebAPI configuration
    added Manager that contains necessary methods
    added controller and action methods that receive messages and send back
    a response

---
+++ WebDiskBrowser.Tests/Managers/FileSystemManagerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebDiskBrowser.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDiskBrowser.Managers.Tests
+{
+	[TestClass()]
+	public class FileSystemManagerTests
+	{
+		[TestMethod()]
+		public void GetDrivesTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void TryGetDirectoriesTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void TryGetFilesTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
---
+++ WebDiskBrowser/Controllers/FileSystemController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebDiskBrowser.Managers;
+
+namespace WebDiskBrowser.Controllers
+{
+    public class FileSystemController : ApiController
+    {
+		private FileSystemManager _fsManager;
+
+		public FileSystemController()
+		{
+			_fsManager = new FileSystemManager();
+		}
+
+		/// <summary>
+		/// Gets an array of strings (directories and files in directory) or warning (if error) + status code as output.
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		public HttpResponseMessage Get(string path)
+		{
+			var warning = "";
+			var files = _fsManager.TryGetFiles(path, out warning);
+			var dirs = _fsManager.TryGetDirectories(path, out warning);
+			if(files == null || dirs == null)
+			{
+				return Request.CreateResponse(HttpStatusCode.NotFound, warning);
+			}
+			else
+			{
+				var output = dirs.Concat(files);
+				return Request.CreateResponse(HttpStatusCode.OK, output);
+			}
+		}
+		public HttpResponseMessage GetDefaultPath()
+		{
+			var desktopPath = _fsManager.GetDesktopPath();
+			return Request.CreateResponse(HttpStatusCode.OK, desktopPath);
+		}
+	}
+}
